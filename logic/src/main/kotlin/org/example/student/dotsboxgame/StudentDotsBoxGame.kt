7af94f8b7db32ab2c6d4afb647ed1d9decc713f9
taraaliz <taralizflavin@gmail.com>
2022-04-06 12:23:56 +0100

    bounding lines now does not return new line objects but existing line objects, next test to fix: testGamePlayBox: When all bounding lines of a box are drawn, the box should now have an owning player ==> expected: not <null>

--- logic/src/main/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGame.kt
+++ logic/src/main/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGame.kt
@@ -130,7 +130,7 @@ class StudentDotsBoxGame (val columns: Int, val rows: Int, players: List<Player>
          * This must be lazy or a getter, otherwise there is a chicken/egg problem with the boxes
          */
         override val boundingLines: Iterable<DotsAndBoxesGame.Line>
-            get() = listOf(StudentLine(boxX, 2 * boxY), StudentLine(boxX, 2 * boxY + 1),
-                StudentLine(boxX, 2* boxY + 2), StudentLine(boxX + 1, 2 * boxY + 1))
+            get() = listOf(lines[boxX, 2 * boxY], lines[boxX, 2 * boxY + 1],
+                lines[boxX, 2* boxY + 2], lines[boxX + 1, 2 * boxY + 1])
     }
 }

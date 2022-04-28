c70bced84f27d0bfffc1c5534d32dfcde1fa527d
taraaliz <taralizflavin@gmail.com>
2022-04-28 13:58:21 +0100

    with textviews and gameviews separate

--- app/src/main/java/uk/ac/bournemouth/ap/dotsandboxes/GameView.kt
+++ app/src/main/java/uk/ac/bournemouth/ap/dotsandboxes/GameView.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import org.example.student.dotsboxgame.EasyAI
 import org.example.student.dotsboxgame.StudentDotsBoxGame
 import org.example.student.dotsboxgame.NamedHuman
@@ -23,6 +26,7 @@ class GameView: View {
 
     private val backCol: Int = Color.rgb(250,250,200)
     private val lineCol: Int = Color.GRAY
+    private val wordCol: Int = Color.BLACK
 
     // Paint variables
     private var dotPaint: Paint = Paint().apply {
@@ -38,59 +42,89 @@ class GameView: View {
         style = Paint.Style.FILL
         color = backCol
     }
-    private var linePaint: Paint = Paint().apply {
-        style = Paint.Style.FILL_AND_STROKE
+    private var borderPaint: Paint = Paint().apply {
+        style = Paint.Style.STROKE
         color = lineCol
+        strokeWidth = 10f
     }
-    private val computerPlayer: EasyAI = EasyAI()
-    private val humanPlayer: NamedHuman = NamedHuman("Tara")
-    private val playersList: List<Player> = listOf(computerPlayer, humanPlayer)
-    val game = StudentDotsBoxGame(columns = 3, rows = 3, players = playersList)
+    private var linePaint: Paint = Paint().apply {
+        style = Paint.Style.STROKE
+        color = lineCol
+        strokeWidth = 10f
+    }
+    private var wordsPaint: Paint = Paint().apply {
+        color = wordCol
+        textAlign = Paint.Align.CENTER
+        textSize = 100f
+        typeface = Typeface.SANS_SERIF
+    }
 
+    val computerPlayer: EasyAI = EasyAI()
+    val humanPlayer: NamedHuman = NamedHuman("Tara")
+    val playersList: List<Player> = listOf(computerPlayer, humanPlayer)
+    val game = StudentDotsBoxGame(columns = 3, rows = 3, players = playersList)
+    val scores = game.getScores()
+    val textPlayer1: String = "Player 1" //+ game.players[0] + scores[0]
+    val textPlayer2: String = "Player 2"/// + game.players[1] + scores[1]
 
 
     override fun onDraw(canvas: Canvas) {
+
         // draw the View
         // Background
         // Measure the size of the canvas, we could take into account padding here
         val canvasWidth = width.toFloat()
         val canvasHeight = height.toFloat()
-
+        val textViewx = canvasWidth / 2f
+        // 16dp from top of view (assuming top is 0)
+        val textView1y = canvasHeight / 2f
+        // 16dp from bottom of view (assuming bottom is 0)
+        //val textView2y = canvasHeight / 4f
         // Padding
         val minX = paddingLeft
         val minY = paddingTop
         val maxX = width - paddingLeft - paddingRight
         val maxY = height - paddingTop - paddingBottom
 
-        var xSep: Float = maxX.toFloat() / game.columns
-        var ySep: Float = maxY.toFloat() / game.rows
+        val xSep: Float = maxX.toFloat() / game.columns
+        val ySep: Float = maxY.toFloat() / game.rows
 
+        // Draw text
+
+//        val textViewPlayer1 = findViewById<TextView?>(R.id.textViewPlayer1)
+//        textViewPlayer1?.text = textPlayer1
+//        val textViewPlayer2 = findViewById<TextView?>(R.id.textViewPlayer2)
+//        textViewPlayer2?.text = textPlayer2
+
+        canvas.drawText("Text", 100f, 100f, wordsPaint)
+        //canvas.drawText(textPlayer2, textViewx, textView2y, wordsPaint)
         // Draw rectangle with drawRect(topleftX, topLeftY, bottomRightX, bottomRightY, Paint)
         // Use Ctrl-P to see the parameters for a function
-        //canvas.drawRect(0f, 0f, canvasWidth, canvasHeight, backPaint)
+        canvas.drawRect(125f, 125f, canvasWidth, canvasHeight, backPaint)
+        canvas.drawRect(125f, 125f, canvasWidth, canvasHeight, borderPaint)
         //val rows = game.rows + 1
         //val columns = game.columns + 1
         // draw lines first
-        for (x in 0..(game.rows)) {
-            for (y in 0..(game.columns)) {
-                //if (y % 2 != 0) {
-                canvas.drawLine(x * xSep + minX, y * ySep + minY, x * xSep,
-                    x * ySep, linePaint)
-                //}
-                // horizontal lines - start where dot starts, end on next column
-                //else {
-                canvas.drawLine(x * xSep + minX, y * ySep + minY, y * xSep - right,
-                    y * ySep - bottom, linePaint)
-                //}
-            }
-        }
-        // draw dots on top of lines
-        for (x in 1..(game.rows)) {
-            for (y in 1..(game.columns)) {
-                // starts where dot starts, ends on next row (vertical)
-                canvas.drawPoint(x * xSep + left, y * ySep + top, dotPaint)
-            }
-        }
+//        for (x in 0..(game.rows)) {
+//            for (y in 0..(game.columns)) {
+//                //if (y % 2 != 0) {
+//                canvas.drawLine(x * xSep + minX, y * ySep + minY, x * xSep,
+//                    x * ySep, linePaint)
+//                //}
+//                // horizontal lines - start where dot starts, end on next column
+//                //else {
+//                canvas.drawLine(x * xSep + minX, y * ySep + minY, y * xSep - right,
+//                    y * ySep - bottom, linePaint)
+//                //}
+//            }
+//        }
+//        // draw dots on top of lines
+//        for (x in 1..(game.rows)) {
+//            for (y in 1..(game.columns)) {
+//                // starts where dot starts, ends on next row (vertical)
+//                canvas.drawPoint(x * xSep + left, y * ySep + top, dotPaint)
+//            }
+//        }
     }
 
     // outputs a 5 x 6 grid of square-shaped red dots
--- app/src/main/java/uk/ac/bournemouth/ap/dotsandboxes/MainActivity.kt
+++ app/src/main/java/uk/ac/bournemouth/ap/dotsandboxes/MainActivity.kt
@@ -2,11 +2,18 @@ package uk.ac.bournemouth.ap.dotsandboxes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.example.student.dotsboxgame.EasyAI
+import org.example.student.dotsboxgame.NamedHuman
+import org.example.student.dotsboxgame.StudentDotsBoxGame
+import uk.ac.bournemouth.ap.dotsandboxeslib.Player
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }

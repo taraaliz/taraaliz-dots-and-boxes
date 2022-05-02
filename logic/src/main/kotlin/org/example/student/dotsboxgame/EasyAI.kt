4f45234761f3e5da5c637441d1f95c036a3afd13
taraaliz <taralizflavin@gmail.com>
2022-05-02 11:36:24 +0100

    Normal AI implemented, not sure how to test it

--- logic/src/main/kotlin/org/example/student/dotsboxgame/EasyAI.kt
+++ logic/src/main/kotlin/org/example/student/dotsboxgame/EasyAI.kt
@@ -7,7 +7,6 @@ import kotlin.random.Random
 class EasyAI: ComputerPlayer() {
   override fun makeMove(game: DotsAndBoxesGame) {
     /** Make a list of lines that have not been drawn, then pick and draw one at random**/
-    //var notDrawnLines: List<DotsAndBoxesGame.Line> = mutableListOf()
     val notDrawnLines = game.lines.filterNot { it.isDrawn }
     val lineToDraw = notDrawnLines.random()
     lineToDraw.drawLine()
--- logic/src/main/kotlin/org/example/student/dotsboxgame/NormalAI.kt
+++ logic/src/main/kotlin/org/example/student/dotsboxgame/NormalAI.kt
@@ -1,4 +1,21 @@
 package org.example.student.dotsboxgame
 
-class NormalAI {
+import uk.ac.bournemouth.ap.dotsandboxeslib.ComputerPlayer
+import uk.ac.bournemouth.ap.dotsandboxeslib.DotsAndBoxesGame
+
+class NormalAI: ComputerPlayer() {
+    override fun makeMove(game: DotsAndBoxesGame) {
+        /** Complete a box if a box can be completed, otherwise move randomly **/
+        val notDrawnLines = game.lines.filterNot { it.isDrawn }
+        var lineToDraw = notDrawnLines.random()
+        for (box in game.boxes) {
+            val drawnLines = box.boundingLines.filter {it.isDrawn}
+            // a box has 3 out of 4 required lines
+            if (drawnLines.size == 3) {
+                // we know this will return only one value but compiler does not know that
+                lineToDraw = box.boundingLines.filterNot {it.isDrawn}.first()
+            }
+        }
+        lineToDraw.drawLine()
+    }
 }
